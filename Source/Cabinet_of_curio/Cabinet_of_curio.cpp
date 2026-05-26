b7efeb1ab9bdbbd5661f748b668af45ac9b7ece8
DanielLyhus <155972586+DanielLyhus@users.noreply.github.com>
2026-05-26 17:11:51 +1000

    updates for a3

---
+++ Source/Cabinet_of_curio/Cabinet_of_curio.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Cabinet_of_curio.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Cabinet_of_curio, "Cabinet_of_curio" );
